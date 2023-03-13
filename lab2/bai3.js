65c5b502970c65121fe209403b82b6a31cd9335a
anh quan <quanpapd08147@fpt.edu.vn>
2023-03-13 17:16:25 +0700

    noplap

--- lab2/bai3.js
+++ lab2/bai3.js
@@ -2,18 +2,18 @@
    let hoTen = prompt("Nhập vào họ và tên:");
    let gioiTinh = prompt("Nhập vào giới tính (Nam/Nữ):");
 
-   // Tách chuỗi họ và tên thành 3 phần họ, tên đệm, tên riêng
+   // tach ho ten ten dem
    let hten = hoTen.split(" ");
    let ho = hten[0];
    let tenDem = hten.slice(1, hten.length - 1).join(" ");
    let ten = hten[hten.length - 1];
 
-   // In ra 3 giá trị họ, tên đệm, tên riêng
+   // In ho ten dem ten
    console.log("Họ: " + ho);
    console.log("Tên đệm: " + tenDem);
    console.log("Tên: " + ten);
 
-   // Thay đổi tên đệm dựa trên giới tính
+   // thay ten dem them gio tinh
    if (gioiTinh.toLowerCase("nam")) {
        tenDem = "Văn";
    } else if (gioiTinh.toLowerCase("nữ")) {
@@ -22,7 +22,7 @@
        console.log("giới tính không hợp lệ ");
    }
 
-   // Tạo tên mới bằng cách ghép các phần tên lại với nhau
 
-   // In ra kết quả tên mới
+
+   // In ra 
    console.log("Họ và tên mới :" + ho + " " + tenDem + " " + ten);
