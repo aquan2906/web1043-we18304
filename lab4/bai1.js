7b9c51f69d7f1b65e0af72f7df4dc505d6eca6ff
anh quan <quanpapd08147@fpt.edu.vn>
2023-04-03 16:35:06 +0700

    naplab

--- lab4/bai1.js
+++ lab4/bai1.js
@@ -1,27 +1,62 @@
+// Tạo class Student
 class Student {
-    constructor(id, first_name, last_name, class_id, email, phone_number,
-        dob){
-            this.id=id;
-            this.first_name=first_name;
-            this.last_name=last_name;
-            this.class_id=class_id;
-            this.email=email;
-            this.phone_number=phone_number;
-            this.dob=dob;
-        }
-        getFullname(){
-            return this.first_name+" "+this.last_name;
-        }
-        getAge(){
-            let today = new Date();
-            let ngaysinh =new Date(this.dob);
-            return today.getFullYear() - ngaysinh.getFullYear();
-        }
-}
-   let sv1 = new Student('1','Pham','A','we08304','anhquan2906',0987650987,'1990/12/24');
-   let sv2 = new Student('2','Nguyen','B','we08304','quanlkk',56787654,'2004/05/14');
-   let sv3 = new Student('3','Le','C','we08304','email3',3454324,'1995/02/24');
-   let sv4 = new Student('4','Ho','D','we08304','email4',345665434,'2010/02/15');
-   let sv5 = new Student('5','Chau','E','we08304','email5',34567654,'2000/04/26');
-
-console.log(sv4.getAge());
+    constructor(id, first_name, last_name, class_id, email, phone_number, dob) {
+      this.id = id;
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.class_id = class_id;
+      this.email = email;
+      this.phone_number = phone_number;
+      this.dob = dob;
+    }
+  
+    // Phương thức lấy tên đầy đủ
+    getFullname() {
+      return this.last_name + ' ' + this.first_name;
+    }
+  
+    // Phương thức tính số tuổi hiện tại theo năm
+    getCurrentAgeByYear() {
+      const today = new Date();
+      const birthDate = new Date(this.dob);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const months = today.getMonth() - birthDate.getMonth();
+      if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
+    }
+  
+    // Phương thức tính số tuổi hiện tại theo ngày
+    getCurrentAgeByDay() {
+      const today = new Date();
+      const birthDate = new Date(this.dob);   
+      const diffTime = Math.abs(today - birthDate);// Math.abs trả về giá trị tuyệt đối
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));// làm tròn lên
+      return diffDays;
+    }
+  }
+  
+  // Tạo danh sách sinh viên
+  const students = [
+    new Student(1, 'John', 'Smith', 1, 'john.smith@gmail.com', '0123456789', '1990/05/01'),
+    new Student(2, 'Mary', 'Johnson', 1, 'mary.johnson@gmail.com', '0123456789', '1993/07/20'),
+    new Student(3, 'David', 'Williams', 2, 'david.williams@gmail.com', '0123456789', '1992/03/29'),
+    new Student(4, 'Emma', 'Taylor', 2, 'emma.taylor@gmail.com', '0123456789', '1995/10/10'),
+    new Student(5, 'Jessica', 'Miller', 1, 'jessica.miller@gmail.com', '0123456789', '1991/12/05')
+  ];
+  
+  // Sắp xếp danh sách sinh viên theo thứ tự từ thấp đến cao theo số tuổi (tính theo ngày)
+  students.sort(function(a, b) {
+    return a.getCurrentAgeByDay() - b.getCurrentAgeByDay();
+  });
+  
+  // In ra danh sách sinh viên theo thứ tự từ thấp đến cao theo số tuổi (tính theo ngày)
+  students.forEach(function(student) {
+    console.log(student.getFullname() + ': ' + student.getCurrentAgeByYear() + ' years old (' + student.getCurrentAgeByDay() + ' days old)');
+  })
+  
+  // In ra thông tin từ BOM về width và height của màn hình
+  console.log('Width: ' + window.innerWidth + ', Height: ' + window.innerHeight);
+  
+  
